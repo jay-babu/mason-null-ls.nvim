89de531eb0a3ccc599523b73d0f91f78e3163a25
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-09-08 22:59:04 -0400

    printing some helpers.

--- plugin/mason-null-ls.lua
+++ plugin/mason-null-ls.lua
@@ -4,3 +4,9 @@ vim.api.nvim_create_autocmd({ 'VimEnter' }, {
 		return require('mason-null-ls').run_on_start
 	end,
 })
+vim.api.nvim_create_user_command('MasonNullLsUpdate', function()
+	require('mason-tool-installer').check_install(true)
+end, { force = true })
+vim.api.nvim_create_user_command('MasonNullLsInstall', function()
+	require('mason-tool-installer').check_install(false)
+end, { force = true })

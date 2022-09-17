f501c9b8723e5422759d63d2963d870b755db4d4
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-09-17 01:45:41 -0400

    changing autocommands to call `mason-null-ls` correctly (#9)

--- plugin/mason-null-ls.lua
+++ plugin/mason-null-ls.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_user_command('MasonNullLsUpdate', function()
-	require('mason-tool-installer').check_install(true)
+	require('mason-null-ls').check_install(true)
 end, { force = true })
 vim.api.nvim_create_user_command('MasonNullLsInstall', function()
-	require('mason-tool-installer').check_install(false)
+	require('mason-null-ls').check_install(false)
 end, { force = true })

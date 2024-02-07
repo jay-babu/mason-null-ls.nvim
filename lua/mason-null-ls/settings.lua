a9765981255ba9000bec548e49850a21a4dc5963
Micah Halter <micah@mehalter.com>
2024-02-07 11:53:38 -0500

    feat: add `methods` to enable or disable specific null-ls source method types

--- lua/mason-null-ls/settings.lua
+++ lua/mason-null-ls/settings.lua
@@ -1,13 +1,30 @@
 local M = {}
 
+---@class MasonNullLsMethods
+---@field diagnostics boolean
+---@field formatting boolean
+---@field code_actions boolean
+---@field completion boolean
+---@field hover boolean
+
 ---@class MasonNullLsSettings
 ---@field handlers table | nil
+---@field ignore_methods MasonNullLsMethods
 ---@field ensure_installed table
 ---@field automatic_installation boolean | table
 local DEFAULT_SETTINGS = {
 	-- A list of sources to automatically install if they're not already installed. Example: { "stylua" }
 	-- This setting has no relation with the `automatic_installation` setting.
 	ensure_installed = {},
+	-- A list of null-ls methods to ignore when calling handlers.
+	-- This setting is useful if some functionality is handled by other plugins such as `conform` and `nvim-lint`
+	methods = {
+		diagnostics = true,
+		formatting = true,
+		code_actions = true,
+		completion = true,
+		hover = true,
+	},
 	-- NOTE: this is left here for future porting in case needed
 	-- Whether sources that are set up (via null-ls) should be automatically installed if they're not already installed.
 	-- This setting has no relation with the `ensure_installed` setting.
@@ -28,6 +45,7 @@ function M.set(opts)
 	M.current = vim.tbl_deep_extend('force', M.current, opts)
 	vim.validate({
 		ensure_installed = { M.current.ensure_installed, 'table', true },
+		methods = { M.current.methods, 'table', true },
 		automatic_installation = { M.current.automatic_installation, { 'boolean', 'table' }, true },
 		handlers = { M.current.handlers, { 'table' }, true },
 	})
